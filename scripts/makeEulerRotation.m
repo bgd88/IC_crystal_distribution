97014382d09a1d0dd2a5d8db13f1e2282f3bb3f8
bgd88 <bdelbridge@fas.harvard.edu>
2019-02-12 12:53:06 -0500

    Replaced with my own version of script which follows D.T. [1998]

--- scripts/makeEulerRotation.m
+++ scripts/makeEulerRotation.m
@@ -1,59 +1,25 @@
 % function R = makeEulerRotation(alpha, beta, gamma)
 %
+% Rotation Matrix Consistent with Dahlen and Tromp [1998]
+%
 % Make the 3x3 rotation matrix for the rotation with Euler angles: 
 % alpha & gamma: [0..2*pi]
 % beta: [0..pi]
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyright (C) 2011, Maarten Moesen                                      % 
-% All rights reserved.                                                    %
-%                                                                         %
-% Author(s): Maarten Moesen                                               %
-% E-mail: moesen.maarten@gmail.com                                        %
-%                                                                         %
-% This file is part of MMTensor.                                          %                                                                        %
-%                                                                         %
-% Redistribution and use in source and binary forms, with or without      %
-% modification, are permitted provided that the following conditions are  %
-% met:                                                                    %
-%    * Redistributions of source code must retain the above copyright     %
-%      notice, this list of conditions and the following disclaimer.      %
-%    * Redistributions in binary form must reproduce the above copyright  %
-%      notice, this list of conditions and the following disclaimer in    %
-%      the documentation and/or other materials provided with the         %
-%      distribution.                                                      %
-%    * Neither the name of the Katholieke Universiteit Leuven nor the     %
-%      names of its contributors may be used to endorse or promote        %
-%      products derived from this software without specific prior written %
-%      permission.                                                        %
-%                                                                         %
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     %
-% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       %
-% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A %
-% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL KATHOLIEKE         %
-% UNIVERSITEIT LEUVEN BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,     %
-% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED %
-% TO, PROCUREMENT OF SUBSTITUTE GOODS ORSERVICES; LOSS OF USE, DATA, OR   %
-% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  %
-% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    %
-% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      %
-% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+% Edited: BD 2/11/19
 function R = makeEulerRotation(alpha, beta, gamma)
 
 
-c1 = cos(alpha);
-s1 = sin(alpha);
+cA = cos(alpha);
+sA = sin(alpha);
 
-c2 = cos(beta);
-s2 = sin(beta);
+cB = cos(beta);
+sB = sin(beta);
 
-c3 = cos(gamma);
-s3 = sin(gamma);
+cG = cos(gamma);
+sG = sin(gamma);
 
-R = [c1*c3-c2*s1*s3, -c1*s3-c3*c2*s1,  s2*s1;
-     c2*c1*s3+c3*s1,  c1*c2*c3-s1*s3, -c1*s2;
-     s3*s2         ,  c3*s2         ,    c2 ];
+R = [ cA*cB*cG-sA*sG,  sA*cB*cG+cA*sG, -sB*cG;
+     -cA*cB*sG-sA*cG, -sA*cB*sG+cA*cG,  sB*sG;
+               cA*sB,           sA*sB,    cB ];
    
 end
