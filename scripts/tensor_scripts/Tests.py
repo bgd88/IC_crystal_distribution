b7fa05a113ca7ebc124ef8ca785cf858d0733817
bgd88 <delbridge@berkeley.edu>
2019-03-05 16:17:18 -0500

    Added unittests for tensor rotations

--- scripts/tensor_scripts/Tests.py
+++ scripts/tensor_scripts/Tests.py
@@ -6,45 +6,32 @@ from unittest import TestCase
 
 from createCubicElasticityMatrix import *
 
-# c11 = 1405.9  * 1.e9 # [Pa]
-# c12 = 1364.8  * 1.e9 # [Pa]
-# c44 =  397.9  * 1.e9 # [Pa]
-# rho =   12.98 * 1.e3 # [kg/m^3]
-#
-# Pressure =  357.5 * 1.e9 # [Pa]
-# Temperature = 6000 # [K]
-#
-# # Create Elasticity 4-Tensor
-# C = createCubicElasticityMatrix(c11, c12, c44)
-#
-# # Display the non-zero components
-# M = displayHookeLawMatrix(C)
-#
-# # Create rotation matrix
-# R = rotation_matrix(np.pi/2)
-#
-# # Rotate Elasticity Tensor
-# Cprime = transform_tensor(C, R)
-#
-# Mprime = displayHookeLawMatrix(Cprime)
 
-
-def are_equal(x, y):
+def are_equal(array_list, N_eps = 10, tol = None):
     ''' Check if x and y are equal to within machine percision.
         If an array is passed, will return False if a single
         element is not within machine percision.
     '''
-    epsilon = np.finfo(float).eps
-    if (np.abs(x-y) > np.maximum(np.abs(x), np.abs(y))*epsilon).any():
-        return False
-    else:
-        return True
-
-def gen_az():
-    return np.random.uniform(0, 2*np.pi)
+    # Get floating point machine percision
+    if tol is None:
+        tol = N_eps * np.finfo(float).eps
+    # Function which compares whether all the elements of two arrays
+    eq = lambda x, y: ( np.abs(x-y) < np.maximum.reduce([np.abs(x), np.abs(y), \
+                                np.ones_like(x)])*tol).all()
+    # compare first array with all the rest
+    bool_list = map(lambda y: eq(array_list[0], y), array_list)
+    # return true if all true, else false
+    return all(bool_list)
 
 class testTensorRotations(TestCase):
+    def _gen_rand_az(self):
+        return np.random.uniform(0, 2*np.pi)
 
+    def _gen_rand_rot(self):
+        return rotation_matrix(*[self._gen_rand_az() for i in np.arange(3)])
+
+    def _gen_rand_mat(self, size=[]):
+        return np.random.uniform(size=size)
 
     def test_right_hand_rule(self):
         """Ensure that rotate Counter Clockwise and sensible"""
@@ -54,31 +41,61 @@ class testTensorRotations(TestCase):
         z_hat = np.array([0,0,1])
 
         # Rotate counter-clockwise az about z-axis
-        az = gen_az()
+        az = self._gen_rand_az()
         caz = np.cos(az)
         saz = np.sin(az)
 
         # Calculate Rotation Matrix
-        Rz45 = rotation_matrix(az)
+        R = rotation_matrix(az)
         # check x_hat
-        x_prime = transform_tensor(x_hat, Rz45)
+        x_prime = transform_tensor(x_hat, R)
         x_true = np.array([caz, saz, 0])
-        assert are_equal(x_prime, x_true), "Z rotation of x_hat not correct"
+        # Brute force Sum over indices
+        x_brute = brute_transform_tensor(x_hat, R)
+        assert are_equal([x_true, x_prime, x_brute]), "Z rotation of x_hat not correct"
 
         # check y_hat
-        y_prime = transform_tensor(y_hat, Rz45)
+        y_prime = transform_tensor(y_hat, R)
         y_true = np.array([-saz, caz, 0])
-        assert are_equal(y_prime, y_true), "Z rotation of y_hat not correct"
+        # Brute force Sum over indices
+        y_brute = brute_transform_tensor(y_hat, R)
+        assert are_equal([y_true, y_prime, y_brute]), "Z rotation of y_hat not correct"
 
         # check y_hat
-        z_prime = transform_tensor(z_hat, Rz45)
+        z_prime = transform_tensor(z_hat, R)
         z_true = np.array([0, 0, 1])
-        assert are_equal(z_prime, z_true), "Z rotation of z_hat not correct"
+        # Brute force Sum over indices
+        z_brute = brute_transform_tensor(z_hat, R)
+        assert are_equal([z_true, z_prime, z_brute]), "Z rotation of z_hat not correct"
 
     def test_vector_rotations(self):
         """Ensure that Code Rotates Vectors Correctly, i.e. rank 1 Tensors"""
-        u = np.random.uniform(size=[3,])
-        R = rotation_matrix(gen_az(), gen_az(), gen_az())
-        u_true = np.matmul(R,u)
-        u_prime = transform_tensor(u, R)
-        assert are_equal(u_prime, u_true), "General rotation of random vec not correct"
+        v = self._gen_rand_mat([3,])
+        R = self._gen_rand_rot()
+        # Simple Matrix Multiply
+        u_true = np.matmul(R,v)
+        # Outer Product
+        u_prime = transform_tensor(v, R)
+        # Brute force Sum over indices
+        u_brute = brute_transform_tensor(v, R)
+        assert are_equal([u_true, u_prime, u_brute]), "General rotation of random vec not correct"
+
+    def test_matrix_rotations(self):
+        """Ensure that Code Rotates Matrices Correctly i.e. rank 2 tensors"""
+        M = self._gen_rand_mat([3,3])
+        R = self._gen_rand_rot()
+        # Simple Matrix Multiply
+        M_true = np.matmul(R,np.matmul(M,R.T))
+        # Outer Product
+        M_prime = transform_tensor(M, R)
+        # Brute force Sum over indices
+        M_brute = brute_transform_tensor(M, R)
+        assert are_equal([M_true, M_prime, M_brute]), "General rotation of random vec not correct"
+
+    def test_outer_product(self):
+        """Ensure that Code Rotates Matrices same as brute force sum i.e. rank 4 tensors"""
+        C = self._gen_rand_mat([3,3,3,3])
+        R = self._gen_rand_rot()
+        C_brute = brute_transform_tensor(C, R)
+        C_prime = transform_tensor(C, R)
+        assert are_equal([C_prime, C_brute]), "not the same"
